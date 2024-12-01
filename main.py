a3f5b23ee89a951955cf5abe807ce0866a80f2fc
dr-monro <dr-monro>
2024-12-01 10:47:26 +0300

    initial commit
    Signed-off-by: dr-monro <dr-monro>

---
+++ main.py
@@ -0,0 +1,86 @@
+import osqp
+import numpy as np
+import scipy as sp
+from scipy import sparse
+
+from mpc_preparations import cast_problem_to_qp
+
+# Discrete time model of a quadcopter
+Ad = sparse.csc_matrix([
+  [1.,      0.,     0., 0., 0., 0., 0.1,     0.,     0.,  0.,     0.,     0.    ],
+  [0.,      1.,     0., 0., 0., 0., 0.,      0.1,    0.,  0.,     0.,     0.    ],
+  [0.,      0.,     1., 0., 0., 0., 0.,      0.,     0.1, 0.,     0.,     0.    ],
+  [0.0488,  0.,     0., 1., 0., 0., 0.0016,  0.,     0.,  0.0992, 0.,     0.    ],
+  [0.,     -0.0488, 0., 0., 1., 0., 0.,     -0.0016, 0.,  0.,     0.0992, 0.    ],
+  [0.,      0.,     0., 0., 0., 1., 0.,      0.,     0.,  0.,     0.,     0.0992],
+  [0.,      0.,     0., 0., 0., 0., 1.,      0.,     0.,  0.,     0.,     0.    ],
+  [0.,      0.,     0., 0., 0., 0., 0.,      1.,     0.,  0.,     0.,     0.    ],
+  [0.,      0.,     0., 0., 0., 0., 0.,      0.,     1.,  0.,     0.,     0.    ],
+  [0.9734,  0.,     0., 0., 0., 0., 0.0488,  0.,     0.,  0.9846, 0.,     0.    ],
+  [0.,     -0.9734, 0., 0., 0., 0., 0.,     -0.0488, 0.,  0.,     0.9846, 0.    ],
+  [0.,      0.,     0., 0., 0., 0., 0.,      0.,     0.,  0.,     0.,     0.9846]
+])
+Bd = sparse.csc_matrix([
+  [0.,      -0.0726,  0.,     0.0726],
+  [-0.0726,  0.,      0.0726, 0.    ],
+  [-0.0152,  0.0152, -0.0152, 0.0152],
+  [-0.,     -0.0006, -0.,     0.0006],
+  [0.0006,   0.,     -0.0006, 0.0000],
+  [0.0106,   0.0106,  0.0106, 0.0106],
+  [0,       -1.4512,  0.,     1.4512],
+  [-1.4512,  0.,      1.4512, 0.    ],
+  [-0.3049,  0.3049, -0.3049, 0.3049],
+  [-0.,     -0.0236,  0.,     0.0236],
+  [0.0236,   0.,     -0.0236, 0.    ],
+  [0.2107,   0.2107,  0.2107, 0.2107]])
+[nx, nu] = Bd.shape
+
+# Constraints
+u0 = 10.5916
+umin = np.array([9.6, 9.6, 9.6, 9.6]) - u0
+umax = np.array([13., 13., 13., 13.]) - u0
+xmin = np.array([-np.pi/6,-np.pi/6,-np.inf,-np.inf,-np.inf,-1.,
+                 -np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf])
+xmax = np.array([ np.pi/6, np.pi/6, np.inf, np.inf, np.inf, np.inf,
+                  np.inf, np.inf, np.inf, np.inf, np.inf, np.inf])
+
+# Objective function
+Q = sparse.diags([0., 0., 10., 10., 10., 10., 0., 0., 0., 5., 5., 5.])
+QN = Q
+R = 0.1*sparse.eye(4)
+
+# Initial and reference states
+x0 = np.zeros(12)
+xr = np.array([0.,0.,1.,0.,0.,0.,0.,0.,0.,0.,0.,0.])
+
+# Prediction horizon
+N = 10
+
+# Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
+# - quadratic objective
+
+P, q, A, l, u = cast_problem_to_qp(Ad, Q, QN, N, R, xr, nx, nu, Bd, x0, xmin,xmax, umin, umax)
+# Create an OSQP object
+prob = osqp.OSQP()
+
+# Setup workspace
+prob.setup(P, q, A, l, u)
+
+# Simulate in closed loop
+nsim = 15
+for i in range(nsim):
+    # Solve
+    res = prob.solve()
+
+    # Check solver status
+    if res.info.status != 'solved':
+        raise ValueError('OSQP did not solve the problem!')
+
+    # Apply first control input to the plant
+    ctrl = res.x[-N*nu:-(N-1)*nu]
+    x0 = Ad@x0 + Bd@ctrl
+
+    # Update initial state
+    l[:nx] = -x0
+    u[:nx] = -x0
+    prob.update(l=l, u=u)
