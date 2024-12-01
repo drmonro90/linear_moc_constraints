2993031b1fcf0106569285418c26e539c13b2a7c
dr-monro <dr-monro>
2024-12-01 11:03:30 +0300

    add: test for quadrotor
    Signed-off-by: dr-monro <dr-monro>

--- mpc_preparations.py
+++ mpc_preparations.py
@@ -3,23 +3,27 @@ import numpy as np
 import scipy as sp
 from scipy import sparse
 
-def cast_problem_to_qp(Ad, Q, QN, N, R, xr, nx, nu, Bd, x0, xmin,xmax, umin, umax):
-    P = sparse.block_diag([sparse.kron(sparse.eye(N), Q), QN,
-                       sparse.kron(sparse.eye(N), R)], format='csc')
+
+def cast_problem_to_qp(Ad, Q, QN, N, R, xr, nx, nu, Bd, x0, xmin, xmax, umin, umax):
+    P = sparse.block_diag(
+        [sparse.kron(sparse.eye(N), Q), QN, sparse.kron(sparse.eye(N), R)], format="csc"
+    )
     # - linear objective
-    q = np.hstack([np.kron(np.ones(N), -Q@xr), -QN@xr, np.zeros(N*nu)])
+    q = np.hstack([np.kron(np.ones(N), -Q @ xr), -QN @ xr, np.zeros(N * nu)])
     # - linear dynamics
-    Ax = sparse.kron(sparse.eye(N+1),-sparse.eye(nx)) + sparse.kron(sparse.eye(N+1, k=-1), Ad)
+    Ax = sparse.kron(sparse.eye(N + 1), -sparse.eye(nx)) + sparse.kron(
+        sparse.eye(N + 1, k=-1), Ad
+    )
     Bu = sparse.kron(sparse.vstack([sparse.csc_matrix((1, N)), sparse.eye(N)]), Bd)
     Aeq = sparse.hstack([Ax, Bu])
-    leq = np.hstack([-x0, np.zeros(N*nx)])
+    leq = np.hstack([-x0, np.zeros(N * nx)])
     ueq = leq
     # - input and state constraints
-    Aineq = sparse.eye((N+1)*nx + N*nu)
-    lineq = np.hstack([np.kron(np.ones(N+1), xmin), np.kron(np.ones(N), umin)])
-    uineq = np.hstack([np.kron(np.ones(N+1), xmax), np.kron(np.ones(N), umax)])
+    Aineq = sparse.eye((N + 1) * nx + N * nu)
+    lineq = np.hstack([np.kron(np.ones(N + 1), xmin), np.kron(np.ones(N), umin)])
+    uineq = np.hstack([np.kron(np.ones(N + 1), xmax), np.kron(np.ones(N), umax)])
     # - OSQP constraints
-    A = sparse.vstack([Aeq, Aineq], format='csc')
+    A = sparse.vstack([Aeq, Aineq], format="csc")
     l = np.hstack([leq, lineq])
     u = np.hstack([ueq, uineq])
     return P, q, A, l, u
