d6f9ae48c3ff4f3c85e10236544f6a13d8e91cb2
dr-monro <dr-monro>
2024-12-04 22:51:17 +0300

    fix: fully parametrized controller
    Signed-off-by: dr-monro <dr-monro>

--- main.py
+++ main.py
@@ -12,42 +12,45 @@ from control.matlab import *
 from mpc_preparations import LinearMpcController
 
 def main():
+    num_dof = 4
     A = np.array([[0, 1], [0,0]])
-    As = np.kron(np.eye(2,dtype=int),A)
+    As = np.kron(np.eye(num_dof,dtype=int),A)
     B = np.array([[0], [1]])
-    Bs = np.block([
+    Bs = np.kron(np.eye(num_dof,dtype=int),B)
+    Bs_ = np.block([
         [B,    np.zeros_like(B)],
         [np.zeros_like(B), B]]
     )
     C = np.array([[1, 0]])
-    Cs =  np.block([
+    Cs = np.kron(np.eye(num_dof,dtype=int),C)
+    Cs_ =  np.block([
         [C, np.zeros_like(C)],
         [np.zeros_like(C), C]
     ])
     D = np.array([[0,]])
-    Ds = np.zeros((2,2))
+    Ds = np.kron(np.eye(num_dof,dtype=int),D)
     model = StateSpace(As,Bs,Cs,Ds)
-    discr = control.matlab.c2d(model,0.1)
+    discr = control.matlab.c2d(model,0.05)
 
 
     Ad, Bd = discr.A, discr.B
     [nx, nu] = Bd.shape
-    umin, umax = 2*[-10], 2*[10]
-    xmin, xmax = 4*[-2], 4*[2]  
+    umin, umax = nu*[-10], nu*[10]
+    xmin, xmax = nx*[-2], nx*[2]  
 
     # Objective function
     Q = sparse.diags(
-        [10.0, 0.0,10.0, 0.0]
+        int(nx/2)*[10.0, 0.0]
     )
     QN = Q
-    R = 0.1 * sparse.eye(2)
+    R = 0.1 * sparse.eye(nu)
 
     # Initial and reference states
-    x0 = np.zeros(4)
-    xr = np.array([1.0, 0.0,1.0, 0.0])
+    x0 = np.zeros(nx)
+    xr = np.array(int(nx/2)*[1.0, 0.0])
 
     # Prediction horizon
-    N = 10
+    N = 20
 
     P = sparse.block_diag(
         [sparse.kron(sparse.eye(N), Q), QN, sparse.kron(sparse.eye(N), R)], format="csc"
