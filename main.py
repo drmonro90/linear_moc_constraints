23cbc9befc9ecb1e74e96a2ccf563f98144e95aa
dr-monro <dr-monro>
2024-12-04 22:34:06 +0300

    2 joints

--- main.py
+++ main.py
@@ -2,85 +2,79 @@ import osqp
 import numpy as np
 import scipy as sp
 from scipy import sparse
+import matplotlib.pyplot as plt
 
-from mpc_preparations import cast_problem_to_qp
+from control import StateSpace
+import control
 
-# Discrete time model of a quadcopter
-Ad = sparse.csc_matrix([
-  [1.,      0.,     0., 0., 0., 0., 0.1,     0.,     0.,  0.,     0.,     0.    ],
-  [0.,      1.,     0., 0., 0., 0., 0.,      0.1,    0.,  0.,     0.,     0.    ],
-  [0.,      0.,     1., 0., 0., 0., 0.,      0.,     0.1, 0.,     0.,     0.    ],
-  [0.0488,  0.,     0., 1., 0., 0., 0.0016,  0.,     0.,  0.0992, 0.,     0.    ],
-  [0.,     -0.0488, 0., 0., 1., 0., 0.,     -0.0016, 0.,  0.,     0.0992, 0.    ],
-  [0.,      0.,     0., 0., 0., 1., 0.,      0.,     0.,  0.,     0.,     0.0992],
-  [0.,      0.,     0., 0., 0., 0., 1.,      0.,     0.,  0.,     0.,     0.    ],
-  [0.,      0.,     0., 0., 0., 0., 0.,      1.,     0.,  0.,     0.,     0.    ],
-  [0.,      0.,     0., 0., 0., 0., 0.,      0.,     1.,  0.,     0.,     0.    ],
-  [0.9734,  0.,     0., 0., 0., 0., 0.0488,  0.,     0.,  0.9846, 0.,     0.    ],
-  [0.,     -0.9734, 0., 0., 0., 0., 0.,     -0.0488, 0.,  0.,     0.9846, 0.    ],
-  [0.,      0.,     0., 0., 0., 0., 0.,      0.,     0.,  0.,     0.,     0.9846]
-])
-Bd = sparse.csc_matrix([
-  [0.,      -0.0726,  0.,     0.0726],
-  [-0.0726,  0.,      0.0726, 0.    ],
-  [-0.0152,  0.0152, -0.0152, 0.0152],
-  [-0.,     -0.0006, -0.,     0.0006],
-  [0.0006,   0.,     -0.0006, 0.0000],
-  [0.0106,   0.0106,  0.0106, 0.0106],
-  [0,       -1.4512,  0.,     1.4512],
-  [-1.4512,  0.,      1.4512, 0.    ],
-  [-0.3049,  0.3049, -0.3049, 0.3049],
-  [-0.,     -0.0236,  0.,     0.0236],
-  [0.0236,   0.,     -0.0236, 0.    ],
-  [0.2107,   0.2107,  0.2107, 0.2107]])
-[nx, nu] = Bd.shape
+from control.matlab import *
 
-# Constraints
-u0 = 10.5916
-umin = np.array([9.6, 9.6, 9.6, 9.6]) - u0
-umax = np.array([13., 13., 13., 13.]) - u0
-xmin = np.array([-np.pi/6,-np.pi/6,-np.inf,-np.inf,-np.inf,-1.,
-                 -np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf])
-xmax = np.array([ np.pi/6, np.pi/6, np.inf, np.inf, np.inf, np.inf,
-                  np.inf, np.inf, np.inf, np.inf, np.inf, np.inf])
+from mpc_preparations import LinearMpcController
 
-# Objective function
-Q = sparse.diags([0., 0., 10., 10., 10., 10., 0., 0., 0., 5., 5., 5.])
-QN = Q
-R = 0.1*sparse.eye(4)
+def main():
+    A = np.array([[0, 1], [0,0]])
+    As = np.kron(np.eye(2,dtype=int),A)
+    B = np.array([[0], [1]])
+    Bs = np.block([
+        [B,    np.zeros_like(B)],
+        [np.zeros_like(B), B]]
+    )
+    C = np.array([[1, 0]])
+    Cs =  np.block([
+        [C, np.zeros_like(C)],
+        [np.zeros_like(C), C]
+    ])
+    D = np.array([[0,]])
+    Ds = np.zeros((2,2))
+    model = StateSpace(As,Bs,Cs,Ds)
+    discr = control.matlab.c2d(model,0.1)
 
-# Initial and reference states
-x0 = np.zeros(12)
-xr = np.array([0.,0.,1.,0.,0.,0.,0.,0.,0.,0.,0.,0.])
 
-# Prediction horizon
-N = 10
+    Ad, Bd = discr.A, discr.B
+    [nx, nu] = Bd.shape
+    umin, umax = 2*[-10], 2*[10]
+    xmin, xmax = 4*[-2], 4*[2]  
 
-# Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
-# - quadratic objective
+    # Objective function
+    Q = sparse.diags(
+        [10.0, 0.0,10.0, 0.0]
+    )
+    QN = Q
+    R = 0.1 * sparse.eye(2)
 
-P, q, A, l, u = cast_problem_to_qp(Ad, Q, QN, N, R, xr, nx, nu, Bd, x0, xmin,xmax, umin, umax)
-# Create an OSQP object
-prob = osqp.OSQP()
+    # Initial and reference states
+    x0 = np.zeros(4)
+    xr = np.array([1.0, 0.0,1.0, 0.0])
 
-# Setup workspace
-prob.setup(P, q, A, l, u)
+    # Prediction horizon
+    N = 10
 
-# Simulate in closed loop
-nsim = 15
-for i in range(nsim):
-    # Solve
-    res = prob.solve()
+    P = sparse.block_diag(
+        [sparse.kron(sparse.eye(N), Q), QN, sparse.kron(sparse.eye(N), R)], format="csc"
+    )
+    # - linear objective
 
-    # Check solver status
-    if res.info.status != 'solved':
-        raise ValueError('OSQP did not solve the problem!')
+    q = np.hstack([np.kron(np.ones(N), -Q @ xr), -QN @ xr, np.zeros(N * nu)])
 
-    # Apply first control input to the plant
-    ctrl = res.x[-N*nu:-(N-1)*nu]
-    x0 = Ad@x0 + Bd@ctrl
+    nsim = 100
 
-    # Update initial state
-    l[:nx] = -x0
-    u[:nx] = -x0
-    prob.update(l=l, u=u)
+    controller = LinearMpcController(Ad, Bd, nx, nu, umin, umax, xmin, xmax, x0, P, q, N)
+    ctrls_ = []
+    states_ = []
+    for i in range(nsim):
+        ctrl = controller.calc_new_control(x0)
+        x0 = Ad @ x0 + Bd @ ctrl
+        ctrls_.append(ctrl)
+        states_.append(x0)
+    plt.plot(ctrls_)
+    plt.plot(states_)
+    plt.show()
+    for i, val in enumerate(ctrls_):
+        assert np.allclose(
+            ctrls_[i], expected_ctrls_[i]
+        ), f"not close {ctrls_[i]} and {expected_ctrls_[i]}"
+
+
+
+if __name__ == "__main__":
+    main()
